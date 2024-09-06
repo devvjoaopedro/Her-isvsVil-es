6b7fc946637dde8954bf93d369380c04a85b50a1
JOAO PEDRO CERQUEIRA DE OLIVEIRA <109909476+devvjoaopedro@users.noreply.github.com>
2024-09-06 20:32:12 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,28 @@
+/**
+ * Array de objetos representando personagens de quadrinhos.
+ * Cada objeto possui as seguintes propriedades:
+ *  - Titulo (string): O nome do personagem.
+ *  - Descricao (string): Uma breve biografia do personagem.
+ *  - Link (string): Um link para a página da Wikipédia do personagem.
+ */
+
+let dados = [
+    {
+      Titulo: "Superman",
+      Descricao: "Nascido no planeta Krypton, Kal-El foi enviado à Terra pelos seus pais antes da destruição de seu planeta natal. Criado por uma família de fazendeiros no Kansas, Clark Kent desenvolveu poderes incríveis sob a luz do Sol amarelo da Terra. Como Superman, ele protege Metrópolis e o mundo com sua força sobre-humana, capacidade de voar, visão de calor, raio-x e outros poderes. Sob a identidade de Clark Kent, ele trabalha como jornalista no Planeta Diário, buscando a verdade e defendendo os mais fracos.",
+      Link: "https://pt.wikipedia.org/wiki/Superman",
+      tags: "Homem de aço Visão de raio x  visão raio x super força"
+    },
+    {
+      Titulo: "Lex Luthor",
+      Descricao: "Lex Luthor é o arqui-inimigo do Superman, um bilionario egocêntrico e brilhante cientista com sede de poder absoluto. Luthor, obcecado por dominar o mundo, vê o Superman como a maior ameaça à sua ambição. Com uma inteligência superior e vastos recursos financeiros, ele trama constantemente planos para derrotar o Homem de Aço e estabelecer sua tirania sobre a humanidade. Luthor é um vilão complexo, motivado por uma mistura de inveja, ódio e um profundo desprezo pela fraqueza humana.",
+      Link: "https://pt.wikipedia.org/wiki/Lex_Luthor",
+      tags: "Bilhonario Cientista Ciêntista Vilão"
+    },
+    {
+      Titulo: "Batman",
+      Descricao: "Batman é um dos super-heróis mais conhecidos e complexos dos quadrinhos. Sob a máscara, está Bruce Wayne, um bilionário traumatizado pela morte de seus pais quando criança. Motivado pela vingança e pela promessa de proteger sua cidade, Gotham, Bruce se submete a um rigoroso treinamento físico e mental, tornando-se o vigilante conhecido como Batman. Sem superpoderes, ele utiliza sua inteligência, habilidades de detetive, força física e uma variedade de equipamentos tecnológicos avançados para combater o crime. Batman é um símbolo de justiça e esperança em Gotham, mas também um homem atormentado por seus demônios internos.",
+      Link: "https://pt.wikipedia.org/wiki/Batman",
+      tags: "Homem-Morcego Homem morcego milhonario solitario"
+    }
+  ];
